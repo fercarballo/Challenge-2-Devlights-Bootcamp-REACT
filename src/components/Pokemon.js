49f125924e2a9cea4f515f0276b8b54781dac3b3
Fernando Carballo <fcarballo65@gmail.com>
2022-05-15 08:08:55 -0300

    details

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -7,7 +7,6 @@ const Pokemon = () => {
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
   const state = useFetch(url);
   const { loading, data } = state;
- 
 
   return (
     <div className="">
@@ -16,12 +15,17 @@ const Pokemon = () => {
       ) : (
         <div>
           <Cards results={data.results} />
-
           <div className="mt-12 h-58 grid grid-cols-2 gap-12 content-start">
-            <button onClick={() => setUrl(data.previous)} className="inline-block px-6 py-4 mb-9 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+            <button
+              onClick={() => setUrl(data.previous)}
+              className="inline-block px-6 py-4 mb-9 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
               Anterior
             </button>
-            <button onClick={() => setUrl(data.next)} className="inline-block px-6 py-4 mb-9 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+            <button
+              onClick={() => setUrl(data.next)}
+              className="inline-block px-6 py-4 mb-9 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
               Siguiente
             </button>
           </div>
